8b7fdfcb6338e0cff1a3c2773e4f7f830e7eb6dd
tiffanyhltse <32942150+tiffanyhltse@users.noreply.github.com>
2019-03-21 20:52:40 -0700

    Update ArrayQueue.java

--- ArrayQueue.java
+++ ArrayQueue.java
@@ -2,7 +2,7 @@ public class ArrayQueue<T> implements Queue<T> {
     protected int head;
     protected int tail;
     protected int size = 10; //size of array
-    protected int count = 0;
+    protected int count = 0; //keeps track of number of elements
     protected Object[] tempArr;
     protected T[] arr;
 
@@ -19,25 +19,26 @@ public class ArrayQueue<T> implements Queue<T> {
         }
         T temp = arr[head];
         head = (head + 1) % arr.length;
-        count --;
+        count--; //decrement every time an element is removed
         return temp;
 
     }
     public void enqueue(T item){ //adds an element to the queue from the end
-        if ((tail + 1) % arr.length == head){
+        if ((tail + 1) % arr.length == head){ //if array is full already, tail circles back to first index of the array (head)
             grow_array();
         }
 
         arr[tail++] = item;
         tail = tail % arr.length;
-        count++;
+        count++; //increment every time an element is added
     }
 
-    protected void grow_array() {
+    protected void grow_array() { //doubles size of the array
         Object[] temp = new Object[arr.length * 2];
         T[] grownArr = (T[]) temp;
         if (head < tail) {
             System.arraycopy(arr, head, grownArr, 0, tail-head);
+            //reset head and tail
             head = 0;
             tail = count;
 
@@ -48,6 +49,7 @@ public class ArrayQueue<T> implements Queue<T> {
 
             //copy [0 to tail]
             System.arraycopy(arr, 0, grownArr, arr.length - head, tail);
+            //reset head and tail
             head = 0;
             tail = count;
         }
